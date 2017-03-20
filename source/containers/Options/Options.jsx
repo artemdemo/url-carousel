e004a28d7bb837b96ab9c53a81e3fe3707200c2c
Artem Demo <artemdemo@gmail.com>
2017-03-20 21:22:37 +0200

    grid for options page

--- source/containers/Options/Options.jsx
+++ source/containers/Options/Options.jsx
@@ -27,17 +27,21 @@ class Options extends Component {
             <div className='options'>
                 <Form onSubmit={() => this.submit()}>
                     <ContentBlock>
-                        <Input
-                            value={this.state.url}
-                            onChange={e => this.setState({
-                                url: e.target.value,
-                                urlError: false,
-                            })}
-                            placeholder='Url' />
-                    </ContentBlock>
-                    <ContentBlock>
-                        <button
-                            className='btn btn-default'>Add</button>
+                        <div className='row'>
+                            <div className='col-xs-9'>
+                                <Input
+                                    value={this.state.url}
+                                    onChange={e => this.setState({
+                                        url: e.target.value,
+                                        urlError: false,
+                                    })}
+                                    placeholder='Url' />
+                            </div>
+                            <div className='col-xs-3'>
+                                <button
+                                    className='btn btn-default'>Add</button>
+                            </div>
+                        </div>
                     </ContentBlock>
                 </Form>
                 <UrlList />
