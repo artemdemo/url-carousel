518e471c574767c925de4670465d8080d81749df
Artem Demo <artemdemo@gmail.com>
2017-03-24 20:56:22 +0300

    carousel in iframe + progress

--- source/containers/Carousel/Carousel.jsx
+++ source/containers/Carousel/Carousel.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { loadUrls } from '../../model/urlList/urlListActions';
 
-const Carousel = () => {
-    return (
-        <div>Carousel!!</div>
-    );
+import './Carousel.less';
+
+const INTERVAL_TIMEOUT = 10000;
+const PROGRESS_STYLE = {
+    transitionDuration: `${INTERVAL_TIMEOUT}ms`,
+    width: '100%',
 };
 
-export default Carousel;
+class Carousel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUrl: '',
+            progressStyle: {},
+        };
+        this.urlIndex = 0;
+        this.urlList = [];
+        this.iframeEl = null;
+    }
+
+    componentDidMount() {
+        const { loadUrls } = this.props;
+        loadUrls();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.urlList = nextProps.urlList.urls;
+
+        if (this.props.urlList.urls !== nextProps.urlList.urls &&
+            nextProps.urlList.urls.length > 0 &&
+            this.state.currentUrl === '') {
+            this.setState({
+                currentUrl: nextProps.urlList.urls[this.urlIndex],
+            });
+            setInterval(
+                () => {
+                    this.urlIndex = this.urlList[this.urlIndex + 1] ?
+                        this.urlIndex + 1 :
+                        0;
+                    this.setState({
+                        currentUrl: this.urlList[this.urlIndex],
+                        progressStyle: {},
+                    });
+                    setTimeout(() => {
+                        this.setState({
+                            progressStyle: PROGRESS_STYLE,
+                        });
+                    }, 100);
+                },
+                INTERVAL_TIMEOUT,
+            );
+            this.setState({
+                progressStyle: PROGRESS_STYLE,
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className='url-carousel'>
+                <div className='url-carousel__progress'
+                     style={this.state.progressStyle} />
+                <iframe className='url-carousel__iframe' src={this.state.currentUrl} />
+            </div>
+        );
+    }
+}
+
+export default connect(
+    state => ({
+        urlList: state.urlList,
+    }),
+    {
+        loadUrls,
+    },
+)(Carousel);
--- source/views/CarouselView/carouselView.jsx
+++ source/views/CarouselView/carouselView.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'babel-polyfill';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from '../../store';
 import Carousel from '../../containers/Carousel/Carousel';
 
 import './carouselView.less';
 
 render(
-    <Carousel />,
+    <Provider store={store}>
+        <Carousel />
+    </Provider>,
     document.getElementById('carousel'),
 );
